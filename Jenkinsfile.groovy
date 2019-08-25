b6a3a3a2c36acbb2eaa4a365a2354969a14fd54e
jsartbaeva <jsartbaeva91@gmail.com>
2019-08-25 15:13:55 -0500

    updates

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,11 +15,18 @@ pipeline{
         stage("Download Terraform"){
             steps{
                 ws("tmp/"){
-                    sh "pwd"
-                    sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
-                    sh "unzip terraform_0.12.7_linux_amd64.zip"
-                    sh "sudo mv terraform /bin"
-                    sh "terraform version"
+                    script {
+                        def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
+                        if (exists) {
+                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                            sh "mv terraform /bin"
+                        } else {
+                            sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                            sh "mv terraform /bin"
+
+                        }
+                    }
                 }
             }
         }
