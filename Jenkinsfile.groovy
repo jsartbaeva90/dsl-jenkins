6f7512b6e13650baed40a7e0205e445227c067bb
jsartbaeva <jsartbaeva91@gmail.com>
2019-08-25 14:41:46 -0500

    added 3 steps

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,11 @@ pipeline{
     stages{
         stage("Run Command"){
             steps{
-                sh"echo Hello"
+                sh'''
+                echo Hello
+                yum install httpd -y
+                ping -c 4 google.com
+                '''
             }
         }
     }
