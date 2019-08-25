a70606636c9ef37dac7ff8739f5a3131e2b4412f
jsartbaeva <jsartbaeva91@gmail.com>
2019-08-25 15:26:14 -0500

    updates

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -31,5 +31,12 @@ pipeline{
                 }
             }
         }
+        stage("Write to a file"){
+            steps{
+                ws("tmp/"){
+                    writeFile text: "Test", file: "TestFile"
+                }
+            }
+        }
     }
 }
