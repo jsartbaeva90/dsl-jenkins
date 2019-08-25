a5d7bcc83c692ffe3e9504a99d1c5e035fd11e82
jsartbaeva <jsartbaeva91@gmail.com>
2019-08-25 15:03:41 -0500

    updates

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,11 +3,11 @@ pipeline{
     stages{
         stage("Run Command"){
             steps{
-                sh'''
+                sh '''
                 set +xe
                 echo Hello
-                ech Error
-                sudo yum install httpd wget -y
+                ech  Error
+                sudo yum install httpd wget unzip -y
                 ping -c 4 google.com
                 '''
             }
@@ -17,6 +17,9 @@ pipeline{
                 ws("tmp/"){
                     sh "pwd"
                     sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+                    sh "unzip terraform_0.12.7_linux_amd64.zip"
+                    sh "sudo mv terraform /bin"
+                    sh "terraform version"
                 }
             }
         }
